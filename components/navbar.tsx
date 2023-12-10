543af44e3a2fafe5893f86cfbaa33e1530c434a4
Evans Maina <124523386+apo-ll@users.noreply.github.com>
2023-12-10 15:06:44 +0000

    Update navbar.tsx and logo.png

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import Logo from "../public/logo.png";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-center py-3">
+    <nav className=" mb-4 flex justify-center pt-3">
       <Image src={Logo} alt="image of logo" width={200} height={200} />
     </nav>
   );
