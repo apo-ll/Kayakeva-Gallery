d05053db420d2d7376274435eef18c9f576daa1b
apo-ll <marsappollo3@gmail.com>
2024-03-26 11:20:10 -0700

    hello

--- app/page.jsx
+++ app/page.jsx
@@ -2,17 +2,13 @@ import RectanglePillows from "@/components/rectangles";
 import SquarecPillows from "@/components/squarec";
 import { SquarePillows } from "@/components/squres";
 import { TabsTrigger, TabsList, TabsContent, Tabs } from "@/components/ui/tabs";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import { Icons } from "@/components/icons";
-import Link from "next/link";
+
 import { Header } from "@/components/header";
 
 export default async function Home() {
   return (
     <main>
-      <div className="flex justify-center mx-auto lg:mt-16 mt-10 mb-12 px-3 ">
+      <div className="flex justify-center container mx-auto lg:mt-16 mt-10 mb-12 px-3 ">
         <Header />
       </div>
 
--- components/header.jsx
+++ components/header.jsx
@@ -2,12 +2,33 @@ import Image from "next/image";
 
 export function Header() {
   return (
-    <Image
-      height={486}
-      width={1331}
-      src="/header2.png"
-      alt="header"
-      className="object-cover"
-    />
+    <div className=" overflow-hidden relative items-center  rounded-[20px] h-[500px] w-full">
+      <Image
+        src="/header.jpg"
+        fill
+        alt="hedaer"
+        className="object-cover h-full w-full"
+      />
+      <div className=" inset-0 absolute bg-orange-500 bg-opacity-30" />
+      <div className="absolute flex py-3 gap-[63px]  flex-col  top-0 lg:pl-8 pl-4 md:pl-4 left-0 *:text-white p">
+        <Image
+          src="/logo.png"
+          width={182}
+          height={90}
+          alt="logo"
+          className=""
+        />
+        <h1 className="lg:text-6xl md:text-5xl text-4xl text-balance lg:w-2/3  ">
+          Limited Time: Buy 6+ items, Get a Free 500{" "}
+          <span className="text-xl">KES</span> Gift!
+        </h1>
+        <div className="flex flex-col gap-3">
+          <p className="lg:text-3xl text-2xl text-gray-100 line-through">
+            600KES
+          </p>
+          <p className="lg:text-5xl text-4xl">500 KES</p>
+        </div>
+      </div>
+    </div>
   );
 }
